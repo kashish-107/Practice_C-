2712e960024b6056676b53e05b4d688f39d0f1ea
Kashish Mittal <kashish.mittal107@gmail.com>
2021-06-04 09:55:15 +0530

    Fixed flaws in singleton design pattern

--- DesignPatterns/Singleton.cpp
+++ DesignPatterns/Singleton.cpp
@@ -1,10 +1,16 @@
+/*
+1. Creational design pattern
+2. Ensures that only one object of its kind exists and 
+provides a single point of access to it for any other code
+*/
+
 #include<iostream>
 using namespace std;
 
 class singleton {
 public:
 	static singleton* getInstance() {
-
+		static singleton* _inst = nullptr;
 		if (!_inst) {
 			cout << "Create instance" << endl;
 			_inst = new singleton();
@@ -12,15 +18,32 @@ public:
 		cout << "Instance" << endl;
 		return _inst;
 	}
+
+	void print() {
+		cout << __FUNCTION__ << endl;
+	}
+
+	singleton(const singleton&) = delete;
+	void operator=(const singleton&) = delete;
+
 private:
-	static singleton* _inst;
+	//static singleton* _inst = nullptr;
 	singleton() = default;
+
+	~singleton() {
+		cout << "In dtor" << endl;
+	}
 };
 
-singleton* singleton::_inst;
+//singleton* singleton::_inst;
 
 int main() {
 	singleton* p = singleton::getInstance();
 	singleton* p1 = singleton::getInstance();
+	p->print();
+
+	//singleton p2 = *p1; //Deleted copy ctor
+	//p1 = p;             //Deleted assignment operator
+	//delete p1;          //Private dtor
 	return 0;
 }
