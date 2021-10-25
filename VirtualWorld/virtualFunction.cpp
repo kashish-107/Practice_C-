08e4ef9cc4a733b2b2cc369d3212377384dbf8f5
kashish-107 <kashish.mittal107@gmail.com>
2021-10-25 23:01:44 +0530

    Comments update

--- VirtualWorld/virtualFunction.cpp
+++ VirtualWorld/virtualFunction.cpp
@@ -4,12 +4,14 @@ The compiler maintains two things to serve this purpose:
 	vtable: A table of function pointers, maintained per class. 
 	vptr: A pointer to vtable, maintained per object instance.i
 
-Important: Irrespective of object is created or not, a static array of function pointer called VTABLE where each cell contains the address of each virtual function contained in that class.
+Important: Irrespective of object is created or not, a static array of function pointer called VTABLE 
+	   where each cell contains the address of each virtual function contained in that class.
 
 
 Compiler adds additional code at two places to maintain and use vptr.
 	1) Code in every constructor. This code sets the vptr of the object being created. This code sets vptr to point to the vtable of the class. 
-	2) wherever a polymorphic call is made, the compiler inserts code to first look for vptr. Once vptr is fetched, vtable of derived class can be accessed. Using vtable, address of derived class function show() is accessed and called. 
+	2) wherever a polymorphic call is made, the compiler inserts code to first look for vptr. 
+		Once vptr is fetched, vtable of derived class can be accessed. Using vtable, address of derived class function show() is accessed and called. 
 
 
 Rules for Virtual Functions:
@@ -17,7 +19,8 @@ Rules for Virtual Functions:
 	- A virtual function can be a friend function of another class.
 	- Virtual functions should be accessed using pointer or reference of base class type to achieve run time polymorphism.
 	- The prototype of virtual functions should be the same in the base as well as derived class.
-	- They are always defined in the base class and overridden in a derived class. It is not mandatory for the derived class to override (or re-define the virtual function), in that case, the base class version of the function is used.
+	- They are always defined in the base class and overridden in a derived class.
+	  It is not mandatory for the derived class to override (or re-define the virtual function), in that case, the base class version of the function is used.
 	- A class may have virtual destructor but it cannot have a virtual constructor.
 */
 
--- VirtualWorld/virtualFunction_Cannot_Be_Static.cpp
+++ VirtualWorld/virtualFunction_Cannot_Be_Static.cpp
@@ -1,5 +1,7 @@
 /*
-Virtual functions are invoked when you have a pointer/reference to an instance of a class. Static functions aren't tied to a particular instance, they're tied to a class. C++ doesn't have pointers-to-class, so there is no scenario in which you could invoke a static function virtually.
+Virtual functions are invoked when you have a pointer/reference to an instance of a class.
+Static functions aren't tied to a particular instance, they're tied to a class. 
+C++ doesn't have pointers-to-class, so there is no scenario in which you could invoke a static function virtually.
 */
 
 #include<iostream>
